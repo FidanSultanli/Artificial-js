95dae029a63ef736c44e0d1753392a2adf9c4cdc
FidanSultanli <fidansultanli325@gmail.com>
2024-10-12 03:05:34 -0700

    added hero and companies section

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,66 @@ navbarBtn.addEventListener("click", function() {
 closeIcon.addEventListener("click", function() {
     mobileMenu.classList.remove("aktiv")
 })
+
+
+
+//Swipper kodlari
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 50,
+        },
+      },
+  }); 
+
+
+
+
+
+
+//   function saytaDaxilOlaniSalamla() {
+//     alert("Welcome, dear")
+//   }
+
+//   function salamla(ad) {
+//      alert("Xos geldin " + ad)
+//   }
+
+
+//   salamla("Fidan")
+
+
+window.addEventListener("load",getMehsullariGetir)
+
+async function getMehsullariGetir() {
+    const unvan = "https://fakestoreapi.com/products"
+
+    try {
+        const internetdenGelenCavab = await fetch(unvan)
+        // console.log(internetdenGelenCavab)
+        const gelenMehsullar = await internetdenGelenCavab.json()
+        console.log(gelenMehsullar)
+    }
+
+    catch(xeta) {
+        console.log(xeta)
+    }
+}
+
+
+
